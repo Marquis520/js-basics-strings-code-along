40f334204b31700d427298be9e6d8e7b33bf00e1
Marquis520 <Marquis520@icloud.com>
2021-03-31 19:45:57 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Latham';
+const welcomeMessage = 'Welcome to Flatbook, Latham!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, LATHAM!'
+const shortGreeting = 'Welcome, L!'
